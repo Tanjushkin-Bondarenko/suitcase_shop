5c0f263612c8ceafe2b80b97c8098e8532116302
Tanja <k3300189ctv@gmail.com>
2025-11-21 12:01:08 +0200

    fix bugs

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-standard"]
+  "extends": ["stylelint-config-standard"],
+   customSyntax: "postcss-scss",
+  plugins: ["stylelint-scss"],
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true, 
+    "no-descending-specificity": null
+  }
 };
